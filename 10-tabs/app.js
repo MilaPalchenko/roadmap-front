fd48362ed6c69a20d48e45b8d65f3de817e08a16
MilaPalchenko <lancelaps@gmail.com>
2024-11-16 22:32:29 +0200

    working js

--- 10-tabs/app.js
+++ 10-tabs/app.js
@@ -1,1 +1,15 @@
-console.log("hello");
+
+function openTab(event, tabName) { 
+  let tabContent = document.querySelectorAll(".tab-content");
+  tabContent.forEach(content => content.style.display = "none");
+
+  var tabBtn = document.querySelectorAll(".tab-btn"); 
+  tabBtn.forEach(btn => btn.classList.remove('active'));
+
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.classList.add('active');
+
+}
+  document.addEventListener("DOMContentLoaded", function() { 
+    document.querySelector(".tab-content").style.display = 'block';
+  });
